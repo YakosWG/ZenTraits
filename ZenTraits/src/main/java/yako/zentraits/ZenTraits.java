ed0454b60d6b34ac862f9a06ffd40364e0e42b43
Yako <yakoswg@gmail.com>
2021-11-28 17:56:15 +0100

    Update version

--- ZenTraits/src/main/java/yako/zentraits/ZenTraits.java
+++ ZenTraits/src/main/java/yako/zentraits/ZenTraits.java
@@ -16,7 +16,7 @@ public class ZenTraits {
 
 	public static final String MODID = "zentraits";
 	public static final String NAME = "ZenTraits";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 	public static final String DEPENCIES = "required-after:crafttweaker;after:tconstruct;after:contenttweaker;after:conarm;after:tconevo";
 
 	public static Logger logger;
